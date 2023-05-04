d868c61f16d456aaaa61dadf5d8e96b27bd1672f
Jami <jamsheduddin03@gmail.com>
2023-05-04 12:50:58 +0600

    chefs featured recipes added

--- src/HomeComponents/Chefs/ChefRecipes.jsx
+++ src/HomeComponents/Chefs/ChefRecipes.jsx
@@ -17,6 +17,7 @@ const ChefRecipes = () => {
     num_recipes,
     years_of_experience,
   } = chefDetail;
+
   console.log(chefDetail);
   console.log(allRecipes);
   console.log(ChefRecipes);
@@ -34,7 +35,7 @@ const ChefRecipes = () => {
     setChefRecipes(recipes);
   }, [allRecipes]);
   return (
-    <div className="bg-[#FE3A4A] text-white px-32 h-[300vh]">
+    <div className="bg-[#FE3A4A] text-white px-32 ">
       <h1>hello recipes:{chefId}</h1>
       <h1>all recipes:{ChefRecipes.length}</h1>
       <div className="flex flex-col lg:flex-row gap-12 mx-auto">
@@ -50,13 +51,39 @@ const ChefRecipes = () => {
             <h1 className="text-5xl font-bold pb-3">{name}</h1>
             <p className="font-light text-xl">{bio}</p>
           </div>
-          <div className="mt-auto font-light text-lg">
+          <div className="mt-auto lg:pt-0 pt-10 font-light text-lg">
             <p>Recipes: {num_recipes}</p>
             <p>Experience: {years_of_experience} years</p>
             <p>
               <FontAwesomeIcon icon={faHeart} /> {likes}
             </p>
           </div>
+        </div>
+      </div>
+      <div className="pt-10 lg:pt-14">
+        <h1 className="text-2xl lg:text-5xl font-bold">
+          Featured recipes from {name}
+        </h1>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 py-8 ">
+          {ChefRecipes.map((recipe) => (
+            <div key={recipe?.id} className="border-2 rounded-lg p-3">
+              <p className="text-2xl font-bold">{recipe?.recipe_name}</p>
+              <span className="block h-[2px] w-36 mt-2  bg-white"></span>
+              <div className="py-3 space-y-2  flex flex-col">
+                <p className="text-xl font-bold">Ingredients</p>
+                <ul className="text-lg font-light">
+                  {recipe?.ingredients?.map((ingredient) => (
+                    <li>- {ingredient}</li>
+                  ))}
+                </ul>
+                <p className="text-xl font-bold">Cooking method</p>
+                <p className="text-lg font-light">{recipe?.cooking_method}</p>
+                <p className="text-xl font-bold mt-auto">
+                  Ratings: {recipe?.rating}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
