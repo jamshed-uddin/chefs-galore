a048a26778b83cac817b7bbcc36087f64e5571be
Jami <jamsheduddin03@gmail.com>
2023-05-04 12:20:54 +0600

    chef banner created

--- src/HomeComponents/Chefs/ChefRecipes.jsx
+++ src/HomeComponents/Chefs/ChefRecipes.jsx
@@ -1,3 +1,5 @@
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 
@@ -32,7 +34,7 @@ const ChefRecipes = () => {
     setChefRecipes(recipes);
   }, [allRecipes]);
   return (
-    <div className="bg-[#FE3A4A] text-white px-32">
+    <div className="bg-[#FE3A4A] text-white px-32 h-[300vh]">
       <h1>hello recipes:{chefId}</h1>
       <h1>all recipes:{ChefRecipes.length}</h1>
       <div className="flex flex-col lg:flex-row gap-12 mx-auto">
@@ -43,9 +45,18 @@ const ChefRecipes = () => {
             alt=""
           />
         </div>
-        <div className="lg:py-10 lg:w-[60%]">
-          <h1 className="text-4xl font-semibold pb-3">{name}</h1>
-          <p className="">{bio}</p>
+        <div className="lg:py-10 lg:w-[60%] lg:mx-auto flex flex-col">
+          <div>
+            <h1 className="text-5xl font-bold pb-3">{name}</h1>
+            <p className="font-light text-xl">{bio}</p>
+          </div>
+          <div className="mt-auto font-light text-lg">
+            <p>Recipes: {num_recipes}</p>
+            <p>Experience: {years_of_experience} years</p>
+            <p>
+              <FontAwesomeIcon icon={faHeart} /> {likes}
+            </p>
+          </div>
         </div>
       </div>
     </div>
